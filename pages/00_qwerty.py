5f4152f6823556b537eb5f35fc11dd3fcb2c2d65
cyj-0723 <cyjmark50723@gmail.com>
2025-06-05 15:48:02 +0900

    Update 00_qwerty.py

--- pages/00_qwerty.py
+++ pages/00_qwerty.py
@@ -1,6 +1,3 @@
 import streamlit as st
 
-# GIF 파일 경로 또는 URL
-gif_path = "funny_cat.gif"  # 로컬 파일 또는 URL 가능
-
-st.image(gif_path, caption="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeXNzbzZ5NG5vMm9lcWtkb3R2bmplZnhvaW1kd2dkZWFocnM3YTVtNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/lMameLIF8voLu8HxWV/giphy.gif", use_column_width=True)
+st.image("https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeXNzbzZ5NG5vMm9lcWtkb3R2bmplZnhvaW1kd2dkZWFocnM3YTVtNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/lMameLIF8voLu8HxWV/giphy.gif")
